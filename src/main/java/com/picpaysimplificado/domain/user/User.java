8bb3c8e65f0715b22dc66e1de7cfd93b39a93c6a
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-06 13:17:47 -0300

    criaçao do modelo de domínio de usuários, onde estão establecidos os campos corretamente e contendo composição com uma enum para identificar o tipo de usuário.

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,100 @@
+package com.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table(name = "user",
+        schema = "public")
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "document",
+            unique = true)
+    private String Document;
+
+    @Column(name = "email",
+            unique = true)
+    private String email;
+
+    @Column(name = "passwsord")
+    private String password;
+
+    @Column(name = "balance")
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+    public User() {}
+
+    public User(UUID id, String firstName, String lastName, String document,
+                String email, String password, BigDecimal balance, UserType type) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getDocument() {
+        return Document;
+    }
+    public void setDocument(String document) {
+        Document = document;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public BigDecimal getBalance() {
+        return balance;
+    }
+    public UserType getType() {
+        return type;
+    }
+    public void setType(UserType type) {
+        this.type = type;
+    }
+}
