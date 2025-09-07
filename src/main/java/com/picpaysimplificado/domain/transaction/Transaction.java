c07e93c3f52e39474f79607f9d818e9b28e38ba9
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-07 11:04:45 -0300

    Implementação do equals e hashcode na classe de Transaction

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -3,12 +3,26 @@ package com.picpaysimplificado.domain.transaction;
 import jakarta.persistence.*;
 import java.util.UUID;
 
-@Entity
-@Table(name = "transaction",
+@Entity(name = "transactions")
+@Table(name = "transactions",
             schema = "public")
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Transaction other)) {
+            return false;
+        }
+        return this.id.equals(other.id);
+    }
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
 }
