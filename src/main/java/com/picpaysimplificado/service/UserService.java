9579f1909f1ad83d0e730bdc346228703e7255cd
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-08 18:08:14 -0300

    Implementação do método para criar usuário

--- src/main/java/com/picpaysimplificado/service/UserService.java
+++ src/main/java/com/picpaysimplificado/service/UserService.java
@@ -1,10 +1,13 @@
 package com.picpaysimplificado.service;
 
+import com.picpaysimplificado.domain.dto.UserRequest;
+import com.picpaysimplificado.domain.dto.UserResponse;
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
 import com.picpaysimplificado.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -14,6 +17,12 @@ public class UserService {
 
     public UserService(UserRepository repository) {
         this.repository = repository;
+    }
+
+    public User createUser(UserRequest userRequest) {
+        User newUser = new User(userRequest);
+        this.save(newUser);
+        return newUser;
     }
 
     public void save(User user) {
